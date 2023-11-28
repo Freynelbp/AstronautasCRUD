5e13f0f189fe2fe5c8f05841a58ed2a78e46898e
Freynelbp <152146734+Freynelbp@users.noreply.github.com>
2023-11-27 20:31:41 -0400

    Add files via upload

---
+++ Readme e Instrucciones SQL/Astronautas Script SQL.sql
@@ -0,0 +1,92 @@
+-- Paso 1 --
+create database AstronautasDB
+
+
+
+-- Paso 2 --
+use AstronautasDB
+
+
+-- Paso 3 --
+-- Creacion de la Tabla Astronautas --
+create table Astronautas(
+	IdAstronauta int identity(1,1) primary key,
+	Foto varchar(max),
+	Nombre varchar(60), 
+	Nacionalidad varchar(50) default ,
+	Descripcion varchar(500),
+	FechaNacimiento Date,
+	FechaFallecimiento Date,
+	Edad int,
+	RedesSociales varchar(500),
+	DetallesMisiones varchar(500),
+	Activo bit default 1
+)
+
+
+-- Paso 4 --
+-- Creacion de los Procedimientos Almacenados --
+create procedure sp_Listar
+as
+begin
+	select * from Astronautas
+end
+
+
+-- Paso 5 --
+create procedure sp_Obtener(
+@IdAstronauta int
+)
+as
+begin
+	select * from Astronautas where IdAstronauta = @IdAstronauta
+end
+
+
+
+-- Paso 6 --
+create procedure sp_Guardar(
+@Foto varchar(max),
+@Nombre varchar(60), 
+@Nacionalidad varchar(50),
+@Descripcion varchar(500),
+@FechaNacimiento Date,
+@FechaFallecimiento Date,
+@Edad int,
+@RedesSociales varchar(500),
+@DetallesMisiones varchar(500)
+)
+as
+begin
+	insert into Astronautas(Foto, Nombre, Nacionalidad, Descripcion, FechaNacimiento, FechaFallecimiento, Edad, RedesSociales, DetallesMisiones)
+	values (@Foto, @Nombre, @Nacionalidad, @Descripcion, @FechaNacimiento, @FechaFallecimiento, @Edad, @RedesSociales, @DetallesMisiones)
+end
+
+
+
+
+
+
+-- Paso 7 --
+create procedure sp_Editar(
+@IdAstronauta int,
+@Foto varchar(max),
+@Nombre varchar(60), 
+@Nacionalidad varchar(50),
+@Descripcion varchar(500),
+@FechaNacimiento Date,
+@FechaFallecimiento Date,
+@Edad int,
+@RedesSociales varchar(500),
+@DetallesMisiones varchar(500),
+@Activo bit
+)
+as
+begin
+	update Astronautas set Foto = @Foto, Nombre = @Nombre, Nacionalidad = @Nacionalidad, Descripcion = @Descripcion,
+	FechaNacimiento = @FechaNacimiento, FechaFallecimiento = @FechaFallecimiento, Edad = @Edad, RedesSociales = @RedesSociales, DetallesMisiones = @DetallesMisiones,
+	Activo = @Activo
+	where IdAstronauta = @IdAstronauta
+end
+
+
---
+++ Readme e Instrucciones SQL/README.txt
@@ -0,0 +1,13 @@
+						--Instrucciones--
+
+-Hacer las instrucciones de la Base de Datos antes de comenzar a hacer cambios o a correr el proyecto
+
+-El Scritpt con todo el query de SQL Server utilizado esta en el mismo repositorio y se llama "Astronautas Script SQL"
+
+-Recuerda cambiar el ConnectionString del archivo appsettings.json. Cambiar el ConnectionStrings por el suyo propio.
+
+Ejemplo de la cadena que utilize:
+"ConnectionStrings": {
+    "CadenaConexion": "Data Source=DESKTOP-QEA0PLL; Initial Catalog=AstronautasDB; Integrated Security=True;"
+  }
+ 
